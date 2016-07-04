aa4f245e88f1e88b1855e73fa6deb56d25474458
symac <web@geobib.fr>
2016-07-04 17:10:30 +0000

    autocommit set to True. Longer but didn't work with latest version of raspbian (investigate ?)

--- build-collection.py
+++ build-collection.py
@@ -2,14 +2,15 @@
 # -*- coding: utf-8 -*-
 
 # Library to deal with database
-import sqlite3
+import sqlite3, sys
 # Library to iterate through folders
 import os
 
 # Variables need to be set
 MP3_FOLDER = "/media/usb/jukidbox"
 
-conn = sqlite3.connect('jukidbox.sqlite')
+#conn = sqlite3.connect('%s/jukidbox.sqlite' % MP3_FOLDER)
+conn = sqlite3.connect('jukidbox.sqlite', isolation_level=None)
 
 # Needed to store UTF8 filename
 conn.text_factory = str
@@ -41,12 +42,13 @@ for subdir in subdirs:
 		# We start by creating an entry for the album
 		c.execute("insert into album (`directory`, `cover`) values (?, ?)",  (subdir, cover))
 		id_album = c.lastrowid
+		print "Ajout album %s" % id_album
 
 		trackNumber = 1
-		for track in tracks:
+		for track in tracks:	
 			c.execute("insert into track (`id_album`, `filename`, `number`) values (?, ?, ?)", (id_album, track, trackNumber))
 			print "%s [%s %s]" % (track[0:30], trackNumber, c.lastrowid)
 			trackNumber += 1
 		# We commit this album 
-		conn.commit()
 conn.close()
+print "Closing"
