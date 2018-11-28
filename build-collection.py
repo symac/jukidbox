e5d91f27b9ca9309437d0dec96fe129583bc34f9
symac <web@geobib.fr>
2018-11-28 06:21:14 +0100

    utilisation d'une classe, nettoyage du code pour construction de la bib

--- build-collection.py
+++ build-collection.py
@@ -7,73 +7,74 @@ import sqlite3, sys
 import os
 
 import logging
+logging.basicConfig(level=logging.INFO)
 
-# Variables need to be set
-MP3_FOLDER = "/media/usb/jukidbox"
-DATABASE = '%s/jukidbox.sqlite' % MP3_FOLDER
-#MP3_FOLDER = "/home/pi/mp3test"
+class collectionBuilder:
+	# Variables need to be set
+	MP3_FOLDER = "/media/usb/jukidbox"
+	DATABASE = '%s/jukidbox.sqlite' % MP3_FOLDER
+	conn = None
+	c = None
 
-conn = sqlite3.connect(DATABASE)
+	def __init__(self):
+		self.setDatabaseConnection()
 
-# Needed to store UTF8 filename
-conn.text_factory = str
-c = conn.cursor()
+	def setDatabaseConnection(self):
+		self.conn = sqlite3.connect(self.DATABASE)
+		# Needed to store UTF8 filename
+		self.conn.text_factory = str
+		self.c = self.conn.cursor()
 
-# We need to build the database if it does not exist
-def is_table(c, table_name):
-    """ This method seems to be working now"""
-    query = "SELECT name from sqlite_sequence WHERE name='" + table_name + "';"
-    cursor = c.execute(query)
-    result = cursor.fetchone()
-    if result == None:
-        return False
-    else:
-        return True
+	def createTableAlbum(self):
+		logging.info("Create Album table")
+		self.c.execute('CREATE TABLE IF NOT EXISTS "album" ("id" INTEGER PRIMARY KEY  AUTOINCREMENT  NOT NULL  UNIQUE , "directory" VARCHAR, "cover" VARCHAR)')
 
-def createTableAlbum(c):
-	logging.info("Create Album table")
-	c.execute('CREATE TABLE IF NOT EXISTS "album" ("id" INTEGER PRIMARY KEY  AUTOINCREMENT  NOT NULL  UNIQUE , "directory" VARCHAR, "cover" VARCHAR)')
+	def createTableTrack(self):
+		logging.info("Create Track table")
+		self.c.execute('CREATE TABLE IF NOT EXISTS "track" ("id" INTEGER PRIMARY KEY  AUTOINCREMENT  NOT NULL  UNIQUE , "id_album" INTEGER NOT NULL , "filename" VARCHAR NOT NULL , "number" INTEGER)')
 
-def createTableTrack(c):
-	logging.info("Create Track table")
-	c.execute('CREATE TABLE IF NOT EXISTS "track" ("id" INTEGER PRIMARY KEY  AUTOINCREMENT  NOT NULL  UNIQUE , "id_album" INTEGER NOT NULL , "filename" VARCHAR NOT NULL , "number" INTEGER)')
+	def resetDatabase(self):
+		self.createTableAlbum()
+		self.createTableTrack()
 
-createTableAlbum(c)
-createTableTrack(c)
+		# We need to remove any information regarding the music DB
+		self.c.execute("delete from album")
+		self.c.execute("delete from track")
+		self.conn.commit()
 
-# We need to remove any information regarding the music DB
-c.execute("delete from album")
-c.execute("delete from track")
+	def updateLibrary(self):
+		self.resetDatabase()
 
-conn.commit()
+		# We are going to iterate through all subdirectories of MP3_FOLDER
+		subdirs = [x[0] for x in os.walk(self.MP3_FOLDER)]
+		for subdir in subdirs:
+			logging.info("# %s #" % subdir)
 
-# We are going to iterate through all subdirectories of MP3_FOLDER
-subdirs = [x[0] for x in os.walk(MP3_FOLDER)]
-for subdir in subdirs:
-	print "# %s #" % subdir
+			tracks = []
+			cover = ""
 
-	tracks = []
-	cover = ""
+			files = os.walk(subdir).next()[2]
+			if (len(files) > 0):
+				for file in sorted(files):
+					if file.upper().endswith("MP3") or file.upper().endswith("OGG"):
+						tracks.append(file)
+					elif file.upper().endswith("JPG"):
+						cover = file
 
-	files = os.walk(subdir).next()[2]
-	if (len(files) > 0):
-		for file in sorted(files):
-			if file.upper().endswith("MP3") or file.upper().endswith("OGG"):
-				tracks.append(file)
-			elif file.upper().endswith("JPG"):
-				cover = file
+				# We start by creating an entry for the album
+				self.c.execute("insert into album (`directory`, `cover`) values (?, ?)",  (subdir, cover))
+				id_album = self.c.lastrowid
+				logging.info("Ajout album %s" % id_album)
 
-		# We start by creating an entry for the album
-		c.execute("insert into album (`directory`, `cover`) values (?, ?)",  (subdir, cover))
-		id_album = c.lastrowid
-		print "Ajout album %s" % id_album
+				trackNumber = 1
+				for track in tracks:
+					self.c.execute("insert into track (`id_album`, `filename`, `number`) values (?, ?, ?)", (id_album, track, trackNumber))
+					logging.info("%s [%s %s]" % (track[0:30], trackNumber, self.c.lastrowid))
+					trackNumber += 1
+				# We commit this album
+		self.conn.commit()
+		self.conn.close()
+		logging.info("Closing connection, update finished")
 
-		trackNumber = 1
-		for track in tracks:
-			c.execute("insert into track (`id_album`, `filename`, `number`) values (?, ?, ?)", (id_album, track, trackNumber))
-			print "%s [%s %s]" % (track[0:30], trackNumber, c.lastrowid)
-			trackNumber += 1
-		# We commit this album
-conn.commit()
-conn.close()
-print "Closing"
+builder = collectionBuilder()
+builder.updateLibrary()
