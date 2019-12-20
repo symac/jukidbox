01ebc237ad2e0ad89687c4356157cc40c75fecc7
symac <web@geobib.fr>
2019-12-20 03:11:59 +0000

    Improve mpg321 killing to prevent two songs playing at same time

--- player.py
+++ player.py
@@ -2,7 +2,9 @@
 # -*- coding: utf-8 -*-
 
 import subprocess, os
+import time
 from subprocess import Popen, PIPE, STDOUT
+import subprocess, signal
 
 class player:
 	active_process = None
@@ -10,17 +12,29 @@ class player:
 	def __init__(self):
 		return
 
-	def playSong(self, songPath):
-		if self.active_process:
-			# On arrête la chanson en cours avant de lancer la suivante
-			self.stopSong()
+	def playSong(self, songPath, force = False):
+		# Killing all mpg321
+		self.stopSong()
+
 		DEVNULL = open(os.devnull, 'wb')
 		self.active_process = subprocess.Popen(["mpg321", "%s" % songPath], stdout=DEVNULL, stderr=STDOUT)
 
 	def stopSong(self):
-		if self.active_process.poll() == None:
-			# We terminate the process only if it is still active
-			self.active_process.terminate()
+		if self.active_process:
+			if self.active_process.poll() == None:
+				# We terminate the process only if it is still active
+				self.active_process.terminate()
+
+		self.cleaningMpg321()
+
+	def cleaningMpg321(self):
+		p = subprocess.Popen(['ps', '-A'], stdout=subprocess.PIPE)
+		out, err = p.communicate()
+		
+		for line in out.splitlines():
+			if 'mpg321' in line:
+				pid = int(line.split(None, 1)[0])
+				os.kill(pid, signal.SIGKILL)
 
 	def isPlaying(self):
 		return self.active_process.poll() == None
