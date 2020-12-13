c96dc2529746570e91d7340909b1378cfe1de6e0
symac <web@geobib.fr>
2020-12-13 14:49:05 +0100

    Logger plus poussé / précis

--- logger.py
+++ logger.py
@@ -1,14 +1,17 @@
 import logging
 
 class logger:
-    lg = None
-    def __init__(self):
-        #logging.basicConfig(filename='example.log', level=logging.DEBUG)
-        logging.basicConfig(level=logging.DEBUG)
-        self.lg = logging.getLogger(__name__)
+	lg = None
+	def __init__(self):
+		root_logger= logging.getLogger()
+		root_logger.setLevel(logging.DEBUG) # or whatever
+		handler = logging.FileHandler('/home/rpi/debug.log', 'w', 'utf-8') # or whatever
+		handler.setFormatter(logging.Formatter('%(asctime)s %(message)s')) # or whatever
+		root_logger.addHandler(handler)
+		self.lg = root_logger
 
-    def __call__(self, msg):
-        self.msg(msg)
+	def __call__(self, msg):
+		self.msg(msg)
 
-    def msg(self, msg):
-        self.lg.debug(msg)
+	def msg(self, msg):
+		self.lg.debug(msg)
